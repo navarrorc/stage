ed70ecd348c18d4c7dffab0a79105bc6ea6d84fb
Roberto <roberto@austincodingacademy.com>
2016-10-29 03:03:31 +0000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('serve', function(){
+    var options = {
+        server: {baseDir: 'public/'},
+        port: process.env.PORT,
+        ui: { port: 8081 },
+        ghostMode: false
+    };
+    browserSync.init(options);
+    gulp.watch('public/**/*.*').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
